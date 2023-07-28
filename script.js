30bbf993dc6e90658f7c0b2e1dfb396a6b86f8b9
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-07-29 01:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,97 +1,169 @@
 //your JS code here. If required.
-const gridItems = document.querySelectorAll(".game > div");
-const gameContainer = document.getElementsByClassName("game")[0];
-const form = document.forms[0];
-const activePlayer = document.getElementById("active-player");
-const messageDiv = document.createElement("div");
+// Start Game events and data changes
+const button = document.querySelectorAll('div>button');
+var player1, player2;
+button[0].addEventListener('click', submitFunc);
 
-let player1, player2;
-let turn;
+function submitFunc() {
+    const player = document.querySelectorAll('div>input');
+    
+    player1 = player[0].value;
+    player2 = player[1].value;
+    if(player1&&player2) {
+        const inputCont = document.getElementById("input-cont");
+        const gridCont = document.getElementById("grid-cont");
+        inputCont.style.display = 'none';
+        gridCont.style.display = 'flex';
 
-form.addEventListener("submit", addPlayers);
-
-function addPlayers(e) {
-  e.preventDefault();
-  player1 = form["name1"].value;
-  player2 = form["name2"].value;
-  turn = 1;
-  updateName();
-  gameContainer.setAttribute("data-game-enabled", "true");
+        gameStart();
+    }
+    else {
+        alert('Please enter both the player names');
+    }
 }
 
-function updateName() {
-  let currentPlayer = turn === 1 ? player1 : player2;
-  activePlayer.innerText = `${currentPlayer}'s turn`;
+function gameStart() {
+    const subHeading = document.getElementsByClassName('message');
+    
+    subHeading[0].innerText = player1+", you're up";
 }
 
-function checkWin() {
-  const winningPatterns = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-  ];
+var turn = 0;
+var gameOn = true;
+var count = 0;
 
-  let winner = null; // Variable to store the winner's name
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
 
-  for (const pattern of winningPatterns) {
-    const [a, b, c] = pattern;
-    if (
-      gridItems[a - 1].innerText !== "" &&
-      gridItems[a - 1].innerText === gridItems[b - 1].innerText &&
-      gridItems[a - 1].innerText === gridItems[c - 1].innerText
-    ) {
-      winner = turn === 1 ? player1 : player2;
-      break;
+one.addEventListener('click', placeYourPick);
+two.addEventListener('click', placeYourPick);
+three.addEventListener('click', placeYourPick);
+four.addEventListener('click', placeYourPick);
+five.addEventListener('click', placeYourPick);
+six.addEventListener('click', placeYourPick);
+seven.addEventListener('click', placeYourPick);
+eight.addEventListener('click', placeYourPick);
+nine.addEventListener('click', placeYourPick);
+
+
+function placeYourPick(event) {
+    if(count!=9&&gameOn) {
+        let playerMark = 'o';
+        if(turn==0) {
+            playerMark = 'x'
+        }
+        event.srcElement.innerText = playerMark;
+        count++;
+		const subHeading = document.getElementsByClassName('message');
+        let won = checkForWin();
+        if(won) {
+            gameOn = false;            
+            if(turn) {
+                subHeading[0].innerText = player2+" congratulations you won!";
+            }
+            else {
+                subHeading[0].innerText = player1+" congratulations you won!";           
+            }
+        }
+		else {
+			
+			if(turn) {
+				subHeading[0].innerText = player1+", you're up";
+			}
+			else subHeading[0].innerText = player2+", you're up";
+		}
+        if(turn) {
+            turn = 0;
+        }
+        else turn++;
     }
-  }
-
-  if (winner) {
-    displayMessage(`${winner} congratulations you won!`);
-    gameContainer.setAttribute("data-game-enabled", "false");
-  } else {
-    // Check for a draw
-    const isDraw = Array.from(gridItems).every((item) => item.innerText !== "");
-    if (isDraw) {
-      displayMessage("It's a draw!");
-      gameContainer.setAttribute("data-game-enabled", "false");
+    else {
+        const subHeading = document.getElementsByClassName('message');
+        subHeading[0].innerText = "Game Completed, click on restart for a new game";        
     }
-  }
+}
+function checkForWin() {
+    if(one.innerText=='x'&&two.innerText=='x'&&three.innerText=='x') {
+        return true;
+    }
+    if(one.innerText=='x'&&four.innerText=='x'&&seven.innerText=='x') {
+        return true;
+    }
+    if(four.innerText=='x'&&five.innerText=='x'&&six.innerText=='x') {
+        return true;
+    }
+    if(seven.innerText=='x'&&eight.innerText=='x'&&nine.innerText=='x') {
+        return true;
+    }
+    if(five.innerText=='x'&&two.innerText=='x'&&eight.innerText=='x') {
+        return true;
+    }
+    if(six.innerText=='x'&&nine.innerText=='x'&&three.innerText=='x') {
+        return true;
+    }
+    if(one.innerText=='x'&&five.innerText=='x'&&nine.innerText=='x') {
+        return true;
+    }
+    if(three.innerText=='x'&&five.innerText=='x'&&seven.innerText=='x') {
+        return true;
+    }
+    
+    if(one.innerText=='o'&&two.innerText=='o'&&three.innerText=='o') {
+        return true;
+    }
+    if(one.innerText=='o'&&four.innerText=='o'&&seven.innerText=='o') {
+        return true;
+    }
+    if(four.innerText=='o'&&five.innerText=='o'&&six.innerText=='o') {
+        return true;
+    }
+    if(seven.innerText=='o'&&eight.innerText=='o'&&nine.innerText=='o') {
+        return true;
+    }
+    if(five.innerText=='o'&&two.innerText=='o'&&eight.innerText=='o') {
+        return true;
+    }
+    if(six.innerText=='o'&&nine.innerText=='o'&&three.innerText=='o') {
+        return true;
+    }
+    if(one.innerText=='o'&&five.innerText=='o'&&nine.innerText=='o') {
+        return true;
+    }
+    if(three.innerText=='o'&&five.innerText=='o'&&seven.innerText=='o') {
+        return true;
+    }
+    return false;
 }
 
-function displayMessage(text) {
-  messageDiv.innerText = text;
-  messageDiv.className = "message";
-  document.body.appendChild(messageDiv);
+button[1].addEventListener('click', clearBoard);
+button[2].addEventListener('click',restartGame);
+
+function clearBoard() {
+    count = 0;
+    one.innerText = '';
+    two.innerText = '';
+    three.innerText = '';
+    four.innerText = '';
+    five.innerText = '';
+    six.innerText = '';
+    seven.innerText = '';
+    eight.innerText = '';
+    nine.innerText = '';
+    turn = 0;
+    gameOn = true;
 }
 
-function onClickItem(event) {
-  if (player1 !== undefined && player2 !== undefined) {
-    const gridItem = event.target;
-    if (gridItem.innerText !== "") {
-      return;
-    }
-    const isGameEnabled = gameContainer.getAttribute("data-game-enabled");
-    if (isGameEnabled === "false") {
-      return;
-    }
-    if (turn === 1) {
-      gridItem.innerText = "X";
-    } else {
-      gridItem.innerText = "O";
-    }
-    checkWin();
-    turn = turn === 1 ? 2 : 1;
-    updateName();
-  } else {
-    alert("Please enter the player's name!");
-  }
-}
-
-for (let i = 0; i < gridItems.length; i++) {
-  gridItems[i].addEventListener("click", onClickItem);
+function restartGame() {
+    const inputCont = document.getElementById("input-cont");
+    const gridCont = document.getElementById("grid-cont");
+    inputCont.style.display = 'block';
+    gridCont.style.display = 'none';
+    clearBoard();
 }
